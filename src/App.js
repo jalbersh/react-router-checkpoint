3504a9ea4c5acc77859d0fde611fa97f343f8607
James Albersheim <james@albersheim.com>
2018-04-04 07:47:18 -0600

    remove commented out code

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import TopNav from './components/TopNav'
 import './App.css'
-//import { bindActionCreators } from 'redux'
-//import { userSignup,userLogout,userLogin } from './actions/auth.actions'
 
 class App extends Component {
 
@@ -14,28 +12,13 @@ class App extends Component {
   }
 
   render() {
-//      console.log('in App.render',this.props)
-//      if (!this.props.auth.logout) {
           return (
             <div>
               <Link to="/login"><TopNav history={this.props.history} /></Link>
             </div>
           )
-//      } else {
-//          return (
-//            <div>
-//            </div>
-//          )
-//      }
   }
 }
-
-//const mapDispatchToProps = dispatch => {
-//   bindActionCreators(
-//      {
-//         userSignup,userLogout,userLogin
-//      }, dispatch)
-//}
 
 function mapStateToProps(state) {
   return {
